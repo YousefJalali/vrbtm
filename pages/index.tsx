980d18f4a6787e1cfe8d02beb144d0c433aaa4bc
Yousef Jalali <yousefjalali@yousefs-MacBook-Pro.local>
2023-01-26 13:52:39 +0300

    highlight omitted words on eye open

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import { TextEditor } from "@/libs/ui/rich-text-editor"
 import { DeltaStatic, Sources } from "quill"
 import { UnprivilegedEditor } from "react-quill"
 import { RxEyeClosed, RxEyeOpen, RxReload } from "react-icons/rx"
+import ReactSelect from "@/libs/ui/select/Select"
 
 export default function Home() {
   // const [showOptions, toggleOptions] = useState(false)
@@ -168,7 +169,7 @@ export default function Home() {
                 <button
                   onClick={() => omitHandler("omit")}
                   disabled={text.trim().length <= 0}
-                  className="mb-2 h-full rounded-lg bg-primary px-5 text-neutral-content"
+                  className="btn-primary btn-sm  btn  mb-2 "
                 >
                   Omit
                 </button>
@@ -191,18 +192,79 @@ export default function Home() {
 
       {/* <div className="mt-6 h-10"> */}
       {isOmit && (
-        <div className="mt-3  flex h-10 space-x-2">
+        <div className="mt-3  flex h-10 justify-end space-x-2">
           <button
             onClick={() => omitHandler("unOmit")}
-            className=" h-full w-full rounded-lg bg-base-200 font-sans text-base-content "
+            className="btn-ghost btn"
           >
             cancel
           </button>
-          <button className="h-full w-full flex-[0_0_70%] rounded-lg bg-primary font-sans text-base-100">
+
+          <label htmlFor="my-modal-6" className="btn-primary btn">
             Add To Notebook
-          </button>
+          </label>
+          {/* <button className="h-full w-full flex-[0_0_70%] rounded-lg bg-primary font-sans text-base-100">
+            Add To Notebook
+          </button> */}
         </div>
       )}
+
+      {/* Put this part before </body> tag */}
+      <input type="checkbox" id="my-modal-6" className="modal-toggle" />
+      <div
+        // htmlFor="my-modal-6"
+        className="modal modal-bottom sm:modal-middle"
+      >
+        <div className="modal-box">
+          <h3 className="text-lg font-bold">Add to Notebook</h3>
+          <p className="py-4">
+            Select a notebook to add the paragraph to or create a new notebook
+          </p>
+
+          {/* <ReactSelect /> */}
+
+          <div className="dropdown dropdown-top w-full">
+            <label
+              tabIndex={0}
+              className="btn m-0 w-full border-0 bg-transparent p-0"
+            >
+              <input
+                readOnly
+                type="text"
+                placeholder="Type here"
+                value="hola"
+                className="input-bordered input w-full text-base-content"
+              />
+            </label>
+            <ul
+              tabIndex={0}
+              className="dropdown-content menu rounded-box w-52 bg-base-100 p-2 shadow"
+            >
+              <li>
+                <a>Item 1</a>
+              </li>
+              <li>
+                <a>Item 2</a>
+              </li>
+            </ul>
+          </div>
+
+          {/* <select className="select-bordered select w-full ">
+            <option disabled selected>
+              Who shot first?
+            </option>
+            <option>Han Solo</option>
+            <option>Greedo</option>
+          </select> */}
+
+          <div className="modal-action">
+            <label htmlFor="my-modal-6" className="btn-ghost btn">
+              Cancel
+            </label>
+            <button className="btn-primary btn">add</button>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
