a35fbddecb5c80e2ecfc7bed146e482da9322c48
Yousef Jalali <yousefjalali@yousefs-MacBook-Pro.local>
2023-01-28 18:38:46 +0300

    add filter

--- components/notebook/SelectNotebook.tsx
+++ components/notebook/SelectNotebook.tsx
@@ -2,7 +2,7 @@ import Modal from "@/libs/ui/modal/Modal"
 import dynamic from "next/dynamic"
 import { SlPlus } from "react-icons/sl"
 import { ImBook } from "react-icons/im"
-import { useId } from "react"
+import { useId, useState } from "react"
 import sortBy from "lodash.sortby"
 
 const NewNotebook = dynamic(() => import("@/components/notebook/NewNotebook"), {
@@ -10,17 +10,19 @@ const NewNotebook = dynamic(() => import("@/components/notebook/NewNotebook"), {
 })
 
 export default function SelectNotebook() {
+  const [filter, setFilter] = useState("")
+
   const id = useId()
 
   const notebooks = [
-    { id, name: "math", color: "#E32F22" },
-    { id, name: "physics", color: "#422F8A" },
-    { id, name: "biology", color: "#5DA842" },
-    { id, name: "history", color: "#3B59A2" },
-    { id, name: "history 2s", color: "#3B59A2" },
-    { id, name: "art", color: "#009e6f" },
-    { id, name: "art 2", color: "#009e6f" },
-    { id, name: "art 3", color: "#009e6f" },
+    { id, name: "math", color: "#E32F22", lastEdit: "28/01/2023" },
+    { id, name: "physics", color: "#422F8A", lastEdit: "28/01/2023" },
+    { id, name: "biology", color: "#5DA842", lastEdit: "29/01/2023" },
+    { id, name: "history", color: "#3B59A2", lastEdit: "28/01/2023" },
+    { id, name: "history 2s", color: "#3B59A2", lastEdit: "28/01/2023" },
+    { id, name: "art", color: "#009e6f", lastEdit: "28/01/2023" },
+    { id, name: "art 2", color: "#009e6f", lastEdit: "28/01/2023" },
+    { id, name: "art 3", color: "#009e6f", lastEdit: "28/01/2023" },
   ]
 
   return (
@@ -37,6 +39,8 @@ export default function SelectNotebook() {
               type="text"
               placeholder="Search…"
               className="input w-full "
+              value={filter}
+              onChange={(e) => setFilter(e.target.value)}
             />
           </div>
 
@@ -49,38 +53,16 @@ export default function SelectNotebook() {
                 </label>
               </li>
 
-              <li className="menu-title sticky top-0 z-10 bg-base-100">
-                <span>Recent</span>
-              </li>
-              {notebooks.splice(0, 1).map((notebook) => (
-                <li key={notebook.id}>
-                  <a>
-                    <ImBook className="w-5" fill={notebook.color} />
-                    <div className="form-control w-full">
-                      <label className="label cursor-pointer p-0 capitalize">
-                        {notebook.name}
-                        <input
-                          type="radio"
-                          name="radio-10"
-                          className="radio checked:bg-blue-500"
-                        />
-                      </label>
-                    </div>
-                  </a>
-                </li>
-              ))}
-
-              {sortBy(notebooks, ["name"]).map((notebook, i, array) => {
-                return (
+              {notebooks
+                .splice(0, 1)
+                .filter((notebook) => notebook.name.includes(filter))
+                .map((notebook, i) => (
                   <>
-                    {(i === 0 ||
-                      notebook.name.charAt(0).toLowerCase() !==
-                        array[i - 1].name.charAt(0).toLowerCase()) && (
+                    {i === 0 && (
                       <li className="menu-title sticky top-0 z-10 bg-base-100">
-                        <span>{notebook.name.charAt(0).toUpperCase()}</span>
+                        <span>Recent</span>
                       </li>
                     )}
-
                     <li key={notebook.id}>
                       <a>
                         <ImBook className="w-5" fill={notebook.color} />
@@ -97,8 +79,39 @@ export default function SelectNotebook() {
                       </a>
                     </li>
                   </>
-                )
-              })}
+                ))}
+
+              {sortBy(notebooks, ["name"])
+                .filter((notebook) => notebook.name.includes(filter))
+                .map((notebook, i, array) => {
+                  return (
+                    <>
+                      {(i === 0 ||
+                        notebook.name.charAt(0).toLowerCase() !==
+                          array[i - 1].name.charAt(0).toLowerCase()) && (
+                        <li className="menu-title sticky top-0 z-10 bg-base-100">
+                          <span>{notebook.name.charAt(0).toUpperCase()}</span>
+                        </li>
+                      )}
+
+                      <li key={notebook.id}>
+                        <a>
+                          <ImBook className="w-5" fill={notebook.color} />
+                          <div className="form-control w-full">
+                            <label className="label cursor-pointer p-0 capitalize">
+                              {notebook.name}
+                              <input
+                                type="radio"
+                                name="radio-10"
+                                className="radio checked:bg-blue-500"
+                              />
+                            </label>
+                          </div>
+                        </a>
+                      </li>
+                    </>
+                  )
+                })}
             </ul>
           </div>
         </div>
