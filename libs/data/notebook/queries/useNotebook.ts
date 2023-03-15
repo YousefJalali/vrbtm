ac49e8bcf76fe18de10cabc0303c3fd47af84e32
Yousef Jalali <yousefjalali@yousefs-MacBook-Pro.local>
2023-03-15 10:48:06 +0300

    enhance 404

--- libs/data/notebook/queries/useNotebook.ts
+++ libs/data/notebook/queries/useNotebook.ts
@@ -2,11 +2,8 @@ import useSWR from "swr"
 import { customFetch, getErrorMessage } from "@/utils"
 import { Notebook } from "@/libs/types"
 
-export const useNotebook = <T = Notebook>(id?: string, query: string = "") => {
-  const url = `/api/notebooks`
-  const path = `/${id}${query ? `?q=${query}` : ""}`
-
-  const { data, error, mutate } = useSWR([url, path], (url) =>
+export const useNotebook = <T = Notebook>(id: string) => {
+  const { data, error, mutate } = useSWR(["/api/notebooks", `/${id}`], (url) =>
     customFetch(url, {})
   )
 
--- pages/404.tsx
+++ pages/404.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 
 export default function Custom404() {
   return (
-    <div className="prose flex min-h-screen flex-col items-center justify-center px-6 py-12">
+    <div className="flex min-h-screen w-full flex-col items-center justify-center px-6 py-12">
       <Image
         className="mb-8 w-1/2 md:w-1/4"
         src="/404.svg"
@@ -12,10 +12,13 @@ export default function Custom404() {
         height={500}
         width={500}
       />
-      <h1 className="text-center">Oops! Page not found.</h1>
-      <span className="text-center">
-        We are sorry, but the page you requested could not be found.
-      </span>
+      <div className="prose">
+        <h1 className="text-center">Oops! Page not found.</h1>
+        <span className="text-center">
+          We are sorry, but the page you requested could not be found.
+        </span>
+      </div>
+
       <Link className="btn-primary btn mt-6 gap-2" href="/">
         <FiArrowLeft />
         Go back home
