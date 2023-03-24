801ecc8b87973a29b594f9f622abedcf67dbce8f
Yousef Jalali <yousefjalali@yousefs-MacBook-Pro.local>
2023-03-24 15:04:38 +0300

    fix key

--- config/firebaseAdmin.ts
+++ config/firebaseAdmin.ts
@@ -3,7 +3,9 @@ import * as firebaseAdmin from "firebase-admin"
 if (!firebaseAdmin.apps.length) {
   firebaseAdmin.initializeApp({
     credential: firebaseAdmin.credential.cert({
-      privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY,
+      privateKey: process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY
+        ? process.env.NEXT_PUBLIC_FIREBASE_PRIVATE_KEY.replace(/\n/gm, "\n")
+        : undefined,
       clientEmail: process.env.NEXT_PUBLIC_FIREBASE_CLIENT_EMAIL,
       projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     }),
