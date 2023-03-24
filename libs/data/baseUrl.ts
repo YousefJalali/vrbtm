d076161a2e55be8e0ea38aeaef98f4b8e65c7567
Yousef Jalali <yousefjalali@yousefs-MacBook-Pro.local>
2023-03-24 14:58:29 +0300

    add auth

--- libs/data/baseUrl.ts
+++ libs/data/baseUrl.ts
@@ -1,5 +1,4 @@
-export function baseUrl() {
-  return process.env.NODE_ENV === "production"
+export const baseUrl =
+  process.env.NODE_ENV === "production"
     ? `https://${process.env.VERCEL_URL}`
     : process.env.NEXT_PUBLIC_DEVELOPMENT_URL
-}
