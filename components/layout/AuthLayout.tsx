bd345e80508c13d332fb59bd2cee102e448bed4c
Jbygoal <jbtgoal@gmail.com>
2023-03-25 23:37:58 +0300

    color bg

--- components/layout/AuthLayout.tsx
+++ components/layout/AuthLayout.tsx
@@ -1,10 +1,15 @@
+import Head from "next/head"
 import { ReactNode } from "react"
 import SideDrawerButton from "./SideDrawerButton"
 
 export default function AuthLayout({ children }: { children: ReactNode }) {
   return (
     <>
-      <header className="fixed top-0 right-0 flex w-full justify-end bg-base-200 p-6 pb-0 lg:hidden">
+      <Head>
+        <meta name="theme-color" content="#F2F7FF"></meta>
+      </Head>
+
+      <header className="fixed top-0 right-0 flex w-full justify-end bg-base-200 px-6 pt-3 pb-0 lg:hidden">
         <SideDrawerButton />
       </header>
 
